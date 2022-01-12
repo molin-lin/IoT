a3848c3bab8a4e9327ebaf997935e655d929ea60
Molin Lin <59522062+molin-lin@users.noreply.github.com>
2022-01-12 15:22:55 +0800

    Add files via upload

---
+++ practice_app.py
@@ -0,0 +1,56 @@
+#These codes as below is clone from https://github.com/miguelgrinberg/flask-video-streaming
+#and create some codes with remarks behind.
+
+#!/usr/bin/env python
+from importlib import import_module
+import os
+from flask import Flask, render_template, Response
+from controllor import control
+
+# import camera driver
+if os.environ.get('CAMERA'):
+    Camera = import_module('camera_' + os.environ['CAMERA']).Camera
+else:
+    from camera import Camera
+
+# Raspberry Pi camera module (requires picamera package)
+from camera_pi import Camera
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
+
+
+def gen(camera):
+    """Video streaming generator function."""
+    yield b'--frame\r\n'
+    while True:
+        frame = camera.get_frame()
+        yield b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n--frame\r\n'
+
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/<string:functionID>')  #Create to sumulate the controlor via GPIO, 2022/1/9, Molin
+def cntl(functionID):
+    message = control(functionID)
+    if message=="on":
+        return render_template('set_on.html')
+    elif message=="off":
+        return render_template('set_off.html')
+    elif message=="video":
+        return render_template('video.html')
+    else:
+        return message
+        
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', threaded=True)
